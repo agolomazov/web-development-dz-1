f985f3b8458db222fc54da39657e0e548f817dbb
6thSence <d.pushkarskaya.pr@gmail.com>
2015-09-29 19:47:46 +0600

    Настроен popup

--- app/js/main.js
+++ app/js/main.js
@@ -6,8 +6,13 @@ $( function(){
                 e.preventDefault();
 
                
-                $('#element_to_pop_up').bPopup();
-
+                // $('#element_to_pop_up').bPopup();
+                     $('#element_to_pop_up').bPopup({
+           speed: 650,
+            transition: 'slideIn',
+	    transitionClose: 'slideBack',
+            modalColor: '#696662'
+        });
             });
 
 
