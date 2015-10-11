2889fb331bf434f426e9c29ffa1c4afd8840a5ee
Anton Golomazov <agolomazov@itloft.ru>
2015-10-11 15:22:28 +0300

    Add routing

--- index.php
+++ index.php
@@ -2,3 +2,22 @@
 
 require_once 'vendor/autoload.php';
 
+$router = new \Klein\Klein();
+
+$router->respond('GET', '/portfolio/?', function () {
+    return 'Страница потрфолио!';
+});
+
+$router->respond('GET', '/contacts/?', function () {
+    return 'Страница контактов!';
+});
+
+$router->respond('GET', '/auth/?', function () {
+    return 'Страница авторизации!';
+});
+
+$router->respond('GET', '/?', function () {
+    return 'Главная страница!';
+});
+
+$router->dispatch();
