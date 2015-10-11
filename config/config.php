41856306a1c101285b746379d7b60d6d048dce80
Anton Golomazov <agolomazov@itloft.ru>
2015-10-11 18:09:37 +0300

    add recaptcha

--- config/config.php
+++ config/config.php
@@ -7,5 +7,11 @@ return array(
         'user' => 'root',
         'password' => '',
         'dbname' => 'dz1'
+    ),
+    'captcha' => array(
+        'public_key' => '6LfYjQ4TAAAAAKHz4KBheMnUqMDuSmvfK2JOaA6w',
+        'secret_key' => '6LfYjQ4TAAAAAGOWnLHDXWrGYMjM9W6HgtajecOF',
+        'url_check_request' => 'https://www.google.com/recaptcha/api/siteverify',
+        'js_script' => "https://www.google.com/recaptcha/api.js"
     )
 );
